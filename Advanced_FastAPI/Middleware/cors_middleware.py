25d12626286fb6a9e2d1947e6bba97bb4396b0e9
Souvick Mazumdar <souvickmazumdar123@gmail.com>
2026-06-16 19:55:15 +0530

    Built in Middleware

---
+++ Advanced_FastAPI/Middleware/cors_middleware.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app=FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin=[
+        'https://my-frontend.com','http://localhost:3000'
+    ],
+    allow_methods=['GET','POST','PUT','DELETE'],
+    allow_headers=['*']
+)
+
+
