1b35c3abd71c56bc29f2e60ef57f90bd7fb06916
Souvick Mazumdar <souvickmazumdar123@gmail.com>
2026-07-07 22:14:17 +0530

    cProfiler and snakeviz

---
+++ Advanced_FastAPI/Module_8/profiling/time-demo.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Request
+import logging
+import time
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] (line %(lineno)d) - %(levelname)s - %(message)s",
+    datefmt="%m-%d-%Y %H:%M:%S"
+)
+
+logger=logging.getLogger('profiler')
+
+app=FastAPI()
+
+@app.middleware('http')
+async def add_timing(request: Request, call_next):
+    start_time=time.time()
+    response=await call_next(request)
+    time_taken=time.time() -start_time
+    logger.info(f"Request to {request.url.path} took {time_taken:.3f} seconds")
+    return response
+
+@app.get("/")
+def home():
+    return {'message': 'Profiler Demo'}
+
+@app.get('/slow')
+async def slow_endpoint():
+    time.sleep(2)
+    return {'message': 'This is a slow endpoint'}
+
+@app.get('/fast')
+async def fast_endpoint():
+    return {'message': 'This is a fast endpoint'}
