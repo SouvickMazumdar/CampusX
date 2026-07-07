1e7f7358ad5616f051026cb76997dd978e57b42d
Souvick Mazumdar <souvickmazumdar123@gmail.com>
2026-07-07 22:41:48 +0530

    line-profiler

---
+++ Advanced_FastAPI/Module_8/profiling/line_profiler/app.py
@@ -0,0 +1,18 @@
+import time
+from fastapi import FastAPI
+
+app=FastAPI()
+
+def computation(n: int):
+    res=sum((i*2) for i in range(n))
+    time.sleep(2)
+    return res
+
+@profile
+def process_data(x: int):
+    return computation(x)
+
+@app.get('/profiling')
+def profiling(a: int):
+    return {'result': process_data(a)}
+
---
+++ Advanced_FastAPI/Module_8/profiling/line_profiler/profiling_test.py
@@ -0,0 +1,14 @@
+from app import process_data
+
+@profile
+def run():
+    process_data(10000)
+
+
+if __name__=='__main__':
+    run()
+
+
+# To run the line profiler
+# kernprof -l -v profiling_test.py
+# It will not work with uvicorn it is having its own kernel "kernprof" that why seperate file is being created
